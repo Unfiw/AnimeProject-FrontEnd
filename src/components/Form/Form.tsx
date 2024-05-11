583e4fa44f9353c3459b140a13caf762f82ac943
Marco <unfiwbd@gmail.com>
2024-05-11 03:42:37 -0600

    LocalStorage and Unprotected fetch

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -2,10 +2,20 @@ import './Form.css'
 import { useState, useEffect } from 'react'
 import Data from './Data'
 
+const API_URL = "http://localhost:3010/"
+
 function Form () {
   const [password, setPassword] = useState<string>("")
   const [email, setEmail] = useState<string>("")
   const [showData, setShowData] = useState<boolean>(false)
+  const [user, setUser] = useState<any>(null)
+
+  useEffect(() => {
+    const userInStorageString = window.localStorage.getItem("user")
+    const userInStorage = JSON.parse(userInStorageString)
+    console.log(userInStorage)
+    setUser(userInStorage)
+  }, [])
 
   useEffect(() =>{
     if(email === 'Marco@gmail.com'){
@@ -15,7 +25,7 @@ function Form () {
 
   const loginData = {
     email: "marco@gmail.com",
-    password: "12345"
+    password: "asd123"
   }
   
   const handleInputChange = (stateUpdate) => {
@@ -25,17 +35,54 @@ function Form () {
   }
 
   const handleOnClick = () => {
-    setShowData(!showData)
+    logIn({email, password})
+    /*setShowData(!showData)
     
     if(email === loginData.email && password === loginData.password){
         alert("Inicio de sesion Exitoso!!")
-    }
+    }*/
     //se llama toogle the flag
-    setShowData(!showData)
+    //setShowData(!showData)
+  }
+
+  const logIn = async ({email, password}: {email:String, password:string}) => {
+    
+    try{
+      const response = await fetch(API_URL + "api/v1/auth/login", {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({email,password}),
+      })
+
+        if(response.status === 200){
+          const data = await response.json()
+          setUser(data)
+          window.localStorage.setItem("user", JSON.stringify(data))
+        } else {
+          alert("Usuario y contraseña incorrecta")
+        }
+
+    }catch(error){
+      console.log(error)
+    }
   }
   
   return (
-    <>
+    <>  {
+            user && (
+              <section className='dataContainer'>
+                {
+                    <>
+                      <p>Email: {user.user.email} </p>
+                      <p>Name: {user.user.name}</p>
+                      <p>Id: {user.user.id}</p>
+                    </>
+                }
+                </section>
+            )
+        }
         <Data email={email} password={password} showData={showData}/>
         <section className="formContainer">
           <span className='inputContainer'>
